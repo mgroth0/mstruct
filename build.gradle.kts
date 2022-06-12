0956f195e9623f26fcbe2508dfbc12471e50f224
mgroth0 <mgroth49@gmail.com>
2022-06-12 16:30:55 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,11 @@
 
 import matt.klib.str.upper
 modtype = LIB
+
+apis(
+  ":k:klib".jvm()
+)
+
 dependencies {
   implementation("org.yaml:snakeyaml:1.28")
-  if (rootDir.name.upper() == "FLOW") {
-    api(project(":k:klib")) {
-      targetConfiguration = "jvmRuntimeElements"
-    }
-  } else {
-    api("matt.k:klib:+")
-  }
 }
