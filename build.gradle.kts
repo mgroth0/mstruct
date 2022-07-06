49ded66b27913166e307d51365222f3bdb8e4b3a
mgroth0 <mgroth49@gmail.com>
2022-07-06 16:46:04 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,7 @@
-import kotlinx.html.currentTimeMillis
-
-val start = currentTimeMillis()
-
-println("1:${currentTimeMillis()-start}")
-
 implementations {
-  println("2:${currentTimeMillis()-start}")
   kjlibGit
-  println("3:${currentTimeMillis()-start}")
   stream
-  println("4:${currentTimeMillis()-start}")
 }
-println("5:${currentTimeMillis()-start}")
 
 implementations(
   libs.snakeyaml,
@@ -19,24 +9,11 @@ implementations(
   dependencies.kotlin("reflect")
 )
 
-println("6:${currentTimeMillis()-start}")
-
 apis {
   klib
   file
 }
 
-println("7:${currentTimeMillis()-start}")
-
 plugins {
-  val pluginStart = System.currentTimeMillis()
-  println("8(plugin):${System.currentTimeMillis()-pluginStart}")
   kotlin("plugin.serialization")
-  println("9(plugin):${System.currentTimeMillis()-pluginStart}")
 }
-
-println("10:${currentTimeMillis()-start}")
-println("11:${currentTimeMillis()-start}")
-println("12:${currentTimeMillis()-start}")
-println("13:${currentTimeMillis()-start}")
-println("14:${currentTimeMillis()-start}")
