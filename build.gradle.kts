abe44e36e6e677a13101b81bb632ca0104dd4976
mgroth0 <mgroth49@gmail.com>
2022-07-20 11:49:42 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,32 @@
+import matt.file.kt
 import matt.kbuild.gbuild.projectDirM
+import matt.klib.lang.cap
+import matt.mstruct.kt.formatKotlinCode
 
-//generateKt(projectDirM + "src" + "main" + "kotlin" + "gen") {
-//  ""
-//}
+generateKt(projectDirM + "src" + "main" + "kotlin" + "gen".kt) {
+
+  class GenProject(p: Project): Project by p {
+	val accessorName = path.substringAfter(":k:").split(":").withIndex().joinToString("") {
+	  if (it.index == 0) it.value else it.value.cap()
+	}
+  }
+
+  val mods = findProject(":k")!!.subprojects.filter {
+	it.modtype != ABSTRACT
+  }.map { GenProject(it) }
+
+
+  matt.mstruct.kt.unnamedKt(
+	"""
+		  enum class KSubProject(val path: String) {
+		     ${
+	  mods.joinToString(",") {
+		it.accessorName + "(\"$path\")"
+	  }
+	}
+		  }
+		"""
+  ).formatKotlinCode(removeRules = listOf(com.pinterest.ktlint.ruleset.standard.EnumEntryNameCaseRule().id))
+
+
+}
