bd89ca4eee76351cfca30f77c4a64bcba81cce75
mgroth0 <mgroth49@gmail.com>
2022-07-20 11:09:30 -0400

    autocommit

--- src/main/kotlin/matt/mstruct/kt/kt.kt
+++ src/main/kotlin/matt/mstruct/kt/kt.kt
@@ -3,6 +3,9 @@ package matt.mstruct.kt
 import com.pinterest.ktlint.core.KtLint
 import com.pinterest.ktlint.core.KtLint.ExperimentalParams
 import com.pinterest.ktlint.core.LintError
+import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.RuleSet
+import com.pinterest.ktlint.ruleset.standard.EnumEntryNameCaseRule
 import com.pinterest.ktlint.ruleset.standard.StandardRuleSetProvider
 import matt.file.UnnamedPackageIsOk
 import matt.klib.log.warn
@@ -15,20 +18,25 @@ fun unnamedKt(s: String) = """
   $s
 """.trimIndent()
 
-private val ktFormatRules by lazy {
-  listOf(
-	StandardRuleSetProvider().get()
-  )
-}
+private val STD_RULE_SET by lazy { StandardRuleSetProvider().get() }
+private val STD_RULE_SETS by lazy { listOf(STD_RULE_SET) }
 
-fun String.formatKotlinCode() = KtLint.format(
+fun String.formatKotlinCode(
+  removeRules: List<Rule> = listOf()
+) = KtLint.format(
   ExperimentalParams(
 	text = this,
 	cb = { e: LintError, corrected: Boolean ->
+
 	  warn("LINT ERROR: $e")
 	  warn("corrected=${corrected}")
 	},
-	ruleSets = ktFormatRules
+	ruleSets = listOf(
+	  RuleSet(
+		"MY-RULE-SET",
+		*STD_RULE_SET.rules.filter { it !in removeRules }.toTypedArray()
+	  )
+	)
   )
 )
 
