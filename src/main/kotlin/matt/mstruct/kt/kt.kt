64681d67e17dcbf740b5b5adf33ff742dd892d7a
mgroth0 <mgroth49@gmail.com>
2022-07-20 11:32:59 -0400

    autocommit

--- src/main/kotlin/matt/mstruct/kt/kt.kt
+++ src/main/kotlin/matt/mstruct/kt/kt.kt
@@ -22,13 +22,13 @@ private val STD_RULE_SETS by lazy { listOf(STD_RULE_SET) }
 fun String.formatKotlinCode(
   removeRules: List<String> = listOf()
 ) = KtLint.format(
-//  EnumEntryNameCaseRule
+  //  EnumEntryNameCaseRule
   ExperimentalParams(
 	text = this,
 	cb = { e: LintError, corrected: Boolean ->
-
-	  warn("LINT ERROR: $e")
-	  warn("corrected=${corrected}")
+	  /*doing nothing here because these seem to be non actionable*/
+	  /*warn("LINT ERROR: $e")
+	  warn("corrected=${corrected}")*/
 	},
 	ruleSets = listOf(
 	  RuleSet(
