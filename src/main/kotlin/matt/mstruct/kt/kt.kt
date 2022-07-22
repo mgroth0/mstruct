54eb9b5210fd385d4a91d433a0535d6935bdb9ee
mgroth0 <mgroth49@gmail.com>
2022-07-22 19:01:46 -0400

    autocommit

--- src/main/kotlin/matt/mstruct/kt/kt.kt
+++ src/main/kotlin/matt/mstruct/kt/kt.kt
@@ -5,15 +5,6 @@ import com.pinterest.ktlint.core.KtLint.ExperimentalParams
 import com.pinterest.ktlint.core.LintError
 import com.pinterest.ktlint.core.RuleSet
 import com.pinterest.ktlint.ruleset.standard.StandardRuleSetProvider
-import matt.file.UnnamedPackageIsOk
-
-fun unnamedKt(s: String) = """
-  @file:${UnnamedPackageIsOk::class.simpleName}
-  
-  import ${UnnamedPackageIsOk::class.qualifiedName}
-  
-  $s
-""".trimIndent()
 
 private val STD_RULE_SET by lazy { StandardRuleSetProvider().get() }
 private val STD_RULE_SETS by lazy { listOf(STD_RULE_SET) }
@@ -38,3 +29,23 @@ fun String.formatKotlinCode(
   )
 )
 
+
+data class KotlinCode(
+  val fileAnnotations: List<Annotation> = listOf(),
+  val packageStatement: String = "",
+  val imports: String = "",
+  val code: String = ""
+) {
+  override fun toString(): String {
+	return """
+	  ${fileAnnotations.joinToString("\n") { "@file:${it::class.simpleName}" }}
+	  $packageStatement
+	  ${fileAnnotations.joinToString("\n") { "import ${it::class.qualifiedName}" }}
+	  $imports
+	  $code
+	""".trimIndent()
+  }
+}
+
+
+fun safeKtName(s: String) = if ("-" in s || " " in s || s[0].isDigit()) "`$s`" else s
