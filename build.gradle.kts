a66da33ec49a84089e982d8742907b44712889c8
mgroth0 <mgroth49@gmail.com>
2022-07-22 19:08:42 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,39 @@
 import matt.file.kt
 import matt.klib.lang.cap
 
-generateKt(matt.mstruct.SourceSets.main,"gen".kt) {
-
-  class GenProject(p: Project): Project by p {
-	val accessorName = path.substringAfter(":k:").split(":").withIndex().joinToString("") {
-	  if (it.index == 0) it.value else it.value.cap()
-	}
-  }
-
-  val mods = findProject(":k")!!.subprojects.filter {
-	it.modtype != ABSTRACT
-  }.map { GenProject(it) }
-
-
-  matt.mstruct.kt.unnamedKt(
-	"""
-		  enum class KSubProject(val path: String) {
-		     ${
-	  mods.joinToString(",") {
-		it.accessorName + "(\"${it.path}\")"
-	  }
-	}
-		  }
-		"""
-  )/*.formatKotlinCode(removeRules = listOf(com.pinterest.ktlint.ruleset.standard.EnumEntryNameCaseRule().id))*/
-
-
-}
+//generateKt(
+//  matt.mstruct.SourceSets.main,
+//  "gen".kt,
+//  run {
+//	matt.mstruct.KotlinCode(
+//
+//	)
+//  }
+//
+//) {
+//
+//  class GenProject(p: Project): Project by p {
+//	val accessorName = path.substringAfter(":k:").split(":").withIndex().joinToString("") {
+//	  if (it.index == 0) it.value else it.value.cap()
+//	}
+//  }
+//
+//  val mods = findProject(":k")!!.subprojects.filter {
+//	it.modtype != ABSTRACT
+//  }.map { GenProject(it) }
+//
+//
+//  matt.mstruct.kt.unnamedKt(
+//	"""
+//		  enum class KSubProject(val path: String) {
+//		     ${
+//	  mods.joinToString(",") {
+//		it.accessorName + "(\"${it.path}\")"
+//	  }
+//	}
+//		  }
+//		"""
+//  )/*.formatKotlinCode(removeRules = listOf(com.pinterest.ktlint.ruleset.standard.EnumEntryNameCaseRule().id))*/
+//
+//
+//}
